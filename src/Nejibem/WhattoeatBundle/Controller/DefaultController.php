147bb5c38a071cbd8061da0574daa0866394dd27
Benjamin Mooney <ben@thedigitalgroup.com.au>
2014-05-10 16:54:05 +1000

    add recipies action to return json

--- src/Nejibem/WhattoeatBundle/Controller/DefaultController.php
+++ src/Nejibem/WhattoeatBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Nejibem\WhattoeatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -10,4 +11,33 @@ class DefaultController extends Controller
     {
         return $this->render('NejibemWhattoeatBundle:Default:index.html.twig');
     }
+
+    public function recipesAction()
+    {
+        $recipesArray = [ 'data' =>
+                            [
+                                [
+                                    "name" =>  "grilled cheese on toast",
+                                    "ingredients" =>  [
+                                        [ "item" => "bread", "amount" => "2", "unit" => "slices"],
+                                        [ "item" => "cheese", "amount" => "2", "unit" => "slices"]
+                                    ]
+                                ]
+                                ,
+                                [
+                                    "name" =>  "salad sandwich",
+                                    "ingredients" =>  [
+                                        [ "item" => "bread", "amount" => "2", "unit" => "slices"],
+                                        [ "item" => "mixed salad", "amount" => "100", "unit" => "grams"]
+                                    ]
+                                ]
+                            ]
+                        ];
+
+        return new JsonResponse($recipesArray);
+    }
+
 }
+
+
+
